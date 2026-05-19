ab1588fb24c042d28bdd18045a5a07d3dc036d8c
sdjs-s-a-gh <164402991+sdjs-s-a-gh@users.noreply.github.com>
2026-05-19 18:40:47 +0100

    finished the sql statements for inserting data

--- insert_data.sql
+++ insert_data.sql
@@ -95,5 +95,65 @@ VALUES ('2',
 
 INSERT INTO dbo.tblProjectResources (project_id, resource_id)
 VALUES ('2', 
+    '2'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('2', 
+    '3'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('2', 
+    '4'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('2', 
+    '5'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('3', 
     '1'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('2', 
+    '3'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('3', 
+    '4'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('3', 
+    '5'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('3', 
+    '6'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('4', 
+    '1'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('4', 
+    '3'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('4', 
+    '5'
+)
+
+INSERT INTO dbo.tblProjectResources (project_id, resource_id)
+VALUES ('4', 
+    '6'
 )
